852a81331b242f67c19508871fa8dac291ee119d
zorth <nicolaszorth@gmail.com>
2025-04-23 21:02:24 +0800

    first commit

---
+++ src/main/java/com/zorth/anima_web/AnimaWebPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.zorth.anima_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimaWebPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimaWebPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zorth/anima_web/AnimaWebPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zorth.anima_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimaWebPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
